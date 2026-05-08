f6bf5b9f500db38fd160b7376c6e95da104b6d49
modar_algayal <mudar.algayal@helsinki.fi>
2026-05-08 16:41:25 +0300

    basic navigation done

--- Image Processing App/src/App.jsx
+++ Image Processing App/src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import { Home } from './pages/Home'
+import { useState } from "react";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-    <Home/>
-    </>
-  )
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
